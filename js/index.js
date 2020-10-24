ae3bc539253383120ef7141d5a16a660caa15ffa
Yoshiki Tsutsui <pudgehunter2@hotmail.com>
2020-10-23 19:05:51 -0500

    pues pude llamar el firebase y usarlos

--- js/index.js
+++ js/index.js
@@ -5,7 +5,12 @@ const registrarButton = document.getElementById('registrarButton');
 const idPresidente = document.getElementById('idPresidente');
 const votarButton = document.getElementById('votarButton');
 
+//los botones apartes que van de primero
+const verCandidatoButton = document.getElementById('verCandidatoButton');
+const verVotacionesButton = document.getElementById('verVotacionesButton');
+
 var database = firebase.database();
+
 
 
 //Siempre va primero esto si lo hace de esta manera
@@ -42,25 +47,73 @@ registrar = () => {
 
 registrarButton.addEventListener('click',registrar);
 
-database.ref('users').on('value',function(data){
-    data.forEach(
-        function(a){
-            let clave = a.key;
-            let valor = a.idCandidato;
-            console.log(clave);
-            console.log(valor);
-        }
-    )
-});
+
 
 votar = () => {
-
     let idPres = idPresidente.value;
+    
+    database.ref('users').on('value',function(data){
+        data.forEach(
+            function(a){
+                let clave = a.key;
+                let nombrePresidente = a.val().nombre;
+                let valor = a.val().idCandidato;
+                console.log(clave);
+                console.log(valor);
+                if(idPres == valor){
+                    alert(nombrePresidente+" "+valor + " " + votos[a]);
+                    votos[a] = votos[a] + 1;
+                }
+            }
+        )
+    });
+    
 
-    if(idPres.equals(data.a.val().getElementById)){
-
-    }
+    
 
 }
 
 votarButton.addEventListener('click',votar);
+
+//los botones apartes tienen una función alert y es...
+
+candidatobutton = () => {
+    database.ref('users').on('value',function(data){
+        data.forEach(
+            function(a){
+                let nombrePresidente = a.val().nombre;
+                let valor = a.val().idCandidato;
+                console.log(valor);
+                alert(nombrePresidente +valor);
+            }
+        )
+        noLoop();
+    });
+}
+
+verCandidatoButton.addEventListener('click',candidatobutton);
+
+votacionesButton = () => {
+    database.ref('users').on('value',function(data){
+        data.forEach(
+            function(a){
+                let nombrePresidente = a.val().nombre;
+                let valor = a.val().idCandidato;
+                console.log(valor);
+                alert(nombrePresidente + votos[a]);
+            }
+        )
+    });
+}
+
+verVotacionesButton.addEventListener('click',votacionesButton);
+
+let votos = [];
+
+database.ref('users').on('value',function(data){
+    data.forEach(
+        function(a){
+            votos[a] = 0;
+        }
+    )
+});
